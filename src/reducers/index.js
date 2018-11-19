a42be36d839eb38aa16e21661df5ce2138648037
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-11-19 10:17:57 +0545

    Connecting Firebase realtime database with Redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,31 @@ import {
   combineReducers
 } from 'redux';
 
+import {
+  todosRef
+} from '../config/firebase';
+import {
+  FETCH_TODOS
+} from '../actions/types';
+
 import data from './dataReducer';
+
+export const addTodo = newTodo => async dispatch => {
+  todosRef.push().set(newTodo);
+};
+
+export const completeTodo = completedTodoId => async dispatch => {
+  todosRef.child(completedTodoId).remove();
+};
+
+export const fetchTodos = () => async dispatch => {
+  todosRef.on("value", snapshot => {
+    dispatch({
+      type: FETCH_TODOS,
+      payload: snapshot.val()
+    });
+  });
+};
 
 export default combineReducers({
   data
