ea2537e25535587cf1f578d4f83f071fcf7c3bf6
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-11-19 10:29:16 +0545

    Update action

---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+import {
+  todosRef
+} from '../config/firebase';
+import {
+  FETCH_TODOS
+} from '../actions/types';
+
+
+export const addTodo = newTodo => async dispatch => {
+  todosRef.push().set(newTodo);
+};
+
+export const completeTodo = completedTodoId => async dispatch => {
+  todosRef.child(completedTodoId).remove();
+};
+
+export const fetchTodos = () => async dispatch => {
+  todosRef.on("value", snapshot => {
+    dispatch({
+      type: FETCH_TODOS,
+      payload: snapshot.val()
+    });
+  });
+};
